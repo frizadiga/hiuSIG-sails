498ffc540b965cb6af8e243b6fcf81a17e68a6ba
frizadiga <frizadiga@gmail.com>
2017-04-21 13:53:59 +0700

    add modal image

--- api/controllers/QueryController.js
+++ api/controllers/QueryController.js
@@ -8,7 +8,7 @@
 module.exports = {
 	getProperties:function(req,res){
 		id = req.param('id');
-		Properties.query('SELECT properties.id, properties.title, files.path FROM properties INNER JOIN files ON properties.no=files.idOwner ORDER BY properties.id',[],function(err,result){
+		Properties.query('SELECT properties.id, properties.idUser, properties.title, files.path FROM properties INNER JOIN files ON properties.no=files.idOwner ORDER BY properties.id',[],function(err,result){
 			if(err){return res.serverError(err);}
 			return res.send(result);
 		});
