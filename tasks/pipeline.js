85e78430c37e55543d8c6b4e0f1b6d2ede0635e2
frizadiga <frizadiga@gmail.com>
2017-05-05 22:56:56 +0700

    add hover-text-blue-grey class abd reorder css load

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -17,6 +17,7 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
+  'css/w3-4.css',
   'css/**/*.css'
 ];
 
