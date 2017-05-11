a856487c2b205301a8b52e4aacb22985db3c135d
frizadiga <frizadiga@gmail.com>
2017-05-11 16:33:31 +0700

    change migrate method

--- config/models.js
+++ config/models.js
@@ -28,7 +28,7 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  migrate:'alter',
+  migrate:'save',
   autoPK:false,
   autoCreatedAt:true,
   autoUpdateAt:true
