e76803be4e82755b1fbdcdf626a4ddfc7251bf62
frizadiga <frizadiga@gmail.com>
2017-05-16 19:51:46 +0700

    add PropertiesTypes model

---
+++ api/models/PropertiesTypes.js
@@ -0,0 +1,25 @@
+/**
+ * PropertiesTypes.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	no:{
+  		type:'integer',
+  		autoIncrement:true,
+  		primaryKey:true
+  	},
+  	id:{
+  		type:'string',
+  		unique:true
+  	},
+  	type:{
+  		type:'string'
+  	}
+  }
+};
+
