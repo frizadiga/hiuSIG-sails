5a97e4a856305d5bcb4547f0f8e8e1709884c182
frizadiga <frizadiga@gmail.com>
2017-05-16 20:11:00 +0700

    add PropertiesTypes controller

---
+++ api/controllers/PropertiesTypesController.js
@@ -0,0 +1,11 @@
+/**
+ * PropertiesTypesController
+ *
+ * @description :: Server-side logic for managing Propertiestypes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
