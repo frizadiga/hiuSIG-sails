a73d519511f941e1120f6317ca208b6dba6d85fc
frizadiga <frizadiga@gmail.com>
2017-05-18 01:04:13 +0700

    add message query filter

--- api/controllers/MessagesController.js
+++ api/controllers/MessagesController.js
@@ -10,7 +10,10 @@ module.exports = {
 		res.view('admin/messages',{layout:"layout-admin"});
 	},
 	messagesList:(req,res)=>{
-		Messages.find({}).populate('receiver').populate('sender').exec((err,data)=>{
+		const receiver = req.session.user;
+		let opts = {};
+		opts = {receiver:receiver.no};
+		Messages.find(opts).populate('receiver').populate('sender').exec((err,data)=>{
 			if(err)return res.negotiate(err);
 			// res.json(data);
 			res.view('admin/messages',{dataMessages:data,layout:'layout-admin'})
