3e7ae70fdfa354072670b86a439e99e7ba5fa1c5
frizadiga <frizadiga@gmail.com>
2017-07-14 10:00:01 +0700

    improve receipts

--- api/controllers/DocumentsController.js
+++ api/controllers/DocumentsController.js
@@ -22,15 +22,19 @@ module.exports = {
 			return res.view('dashboards/documents/kuitansi',{dataKuitansi:null,title:'Kuitansi',layout:'layout-dashboards'});
 		}else{
 			Payments.findOne({id:params.id}).populate('listing').populate('customer').populate('agent').exec((err,data)=>{
+				if(err)return res.serverError(err);
 				// if(data.id){data.id=data.id.replace('pyt','kwt-') || '-'}
 				if(params.paymentType === 'DP'){
+					data.noKwt=data.id.replace('pyt','KWT-DP-') || '-'
 					data.uangSejumlah = data.dp;
-					return res.view('dashboards/documents/kuitansi',{dataKuitansi:data,title:'Kuitansi',layout:'layout-dashboards'});
+					data.untukPembayaran = 'Uang Muka';
 				}
 				if(params.paymentType === 'Pelunasan'){
+					data.noKwt=data.id.replace('pyt','KWT-PL-') || '-'
 					data.uangSejumlah = data.remainingPayment;
-					return res.view('dashboards/documents/kuitansi',{dataKuitansi:data,title:'Kuitansi',layout:'layout-dashboards'});
+					data.untukPembayaran = 'Pelunasan';
 				}
+				return res.view('dashboards/documents/kuitansi',{dataKuitansi:data,title:'Kuitansi',layout:'layout-dashboards'});
 			});
 		}
 	}
