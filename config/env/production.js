49cba681e4c3209956285fd040e8bd9005c33a72
frizadiga <frizadiga@gmail.com>
2017-07-30 17:06:12 +0700

    change production remote database

--- config/env/production.js
+++ config/env/production.js
@@ -22,7 +22,7 @@ module.exports = {
   // },
 
   models: {
-    connection: 'freesqldatabase'
+    connection: 'heliohost'
   },
 
   /***************************************************************************
